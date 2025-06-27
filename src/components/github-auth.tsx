b5261aaa91428fe7081a911088eee6c4d86749ea
Ashley Wolf <ashleywolf@github.com>
2025-06-27 20:45:11 +0000

    Generated by Spark: Fix all reported errors. Fix all the following errors: Uncaught ReferenceError: useState is not defined

--- src/components/github-auth.tsx
+++ src/components/github-auth.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/auth-context";
 import { Button } from "@/components/ui/button";
 import {
